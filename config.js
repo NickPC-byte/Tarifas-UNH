95f481966d8e07d2cef737e885323b62df9d0786
NickPC-byte <lucio.palomino@unh.edu.pe>
2025-11-28 11:43:12 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-// ID del Google Sheets proporcionado por el usuario
+// ID del Google Sheets
 const GOOGLE_SHEET_ID = "14_ROarwRyo5tsdjQZeMa_hY1TVuyJtHrXzgpILDf9NM";
 
-// GID de la hoja exacta donde están las tarifas
+// GID de la pestaña principal
 const GOOGLE_SHEET_GID = "1276577330";
 
-// URL para descargar el CSV con la data
+// URL CSV
 const SHEET_CSV_URL = `https://docs.google.com/spreadsheets/d/${GOOGLE_SHEET_ID}/export?format=csv&gid=${GOOGLE_SHEET_GID}`;
