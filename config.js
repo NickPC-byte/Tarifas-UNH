912282371ab4c5edfc537b9433c5cb9bede164ad
NickPC-byte <lucio.palomino@unh.edu.pe>
2026-01-20 16:06:26 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,4 +5,4 @@ const GOOGLE_SHEET_ID = "14_ROarwRyo5tsdjQZeMa_hY1TVuyJtHrXzgpILDf9NM";
 const GOOGLE_SHEET_GID = "1276577330";
 
 // URL CSV
-const SHEET_CSV_URL = `https://docs.google.com/spreadsheets/d/${GOOGLE_SHEET_ID}/export?format=csv&gid=${GOOGLE_SHEET_GID}`;
+const SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSvu5g5Ubgccjk_GafzxPj7J1WQYvlFLWD4OURUQQ8BgTKREDec4R5aXNRoqOgU9avsFvggsWfafWyS/pub?output=csv";
